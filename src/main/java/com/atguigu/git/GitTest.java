68b28b3aa120a365c6f66ebbd25fc913e0bbc55c
Joe <getingyu1229@163.com>
2021-09-28 10:46:57 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Ge
+ * @create 2021-09-28-10:42
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello.git");
+    }
+}
